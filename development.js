1aa13111b518cf45b8bcd78c315b11a5746a5a18
Seth Tippetts <sethtippetts@gmail.com>
2018-03-27 22:05:08 -0600

    fix: use micro PORT

--- development.js
+++ development.js
@@ -1,7 +1,8 @@
 require('dotenv').load()
 const { microGraphiql } = require('apollo-server-micro')
 const { router, get, post, options } = require('microrouter')
-const { http, schema } = require('./index')
+const http = require('./index')
+const schema = require('./schema')
 
 module.exports = router(
   get('/', microGraphiql({ schema, endpointURL: '/' })),
