ea307aed231d9e3d7f4f8e3aa07c9c7e9be63d90
Seth Tippetts <sethtippetts@gmail.com>
2018-03-28 11:18:40 -0600

    refactor: remove cors requirement

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const { graphql } = require('graphql')
-const { json, send } = require('micro')
-const { router, get, post, options } = require('microrouter')
-const { microGraphiql } = require('apollo-server-micro')
+const { router, get, post } = require('microrouter')
+const { microGraphql, microGraphiql } = require('apollo-server-micro')
 
 const schema = require('./schema')
-const http = async (req, res) => {
-  res.setHeader('Access-Control-Max-Age', 60 * 60 * 24)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', [ 'POST', 'OPTIONS' ])
-  res.setHeader('Access-Control-Allow-Headers', [ 'X-Apollo-Tracing', 'credentials', 'X-Requested-With', 'Access-Control-Allow-Origin', 'X-HTTP-Method-Override', 'Content-Type', 'Authorization', 'Accept' ])
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  if (req.method === 'OPTIONS') {
-    console.log('return early for options')
-    return send(res, 204)
-  }
-  const { query, variables } = await json(req)
-  console.log('return real thing', query, variables)
-  return graphql(schema, query, {}, { viewer: { roles: ['admin'] } }, variables)
-}
 
 module.exports = router(
-  get('/', microGraphiql({ schema, endpointURL: '/' })),
-  options('/', http),
-  post('/', http)
+  get('/graphql', microGraphiql({ schema, endpointURL: '/graphql' })),
+  post('/graphql', microGraphql({ schema, context: { viewer: { roles: ['admin'] } } }))
 )
