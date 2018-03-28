52a56d79d596ed8abfe51a8b3e82e87e86519738
Seth Tippetts <sethtippetts@gmail.com>
2018-03-27 18:19:38 -0600

    feature: development cors support

--- development.js
+++ development.js
@@ -1,9 +1,10 @@
 require('dotenv').load()
 const { microGraphiql } = require('apollo-server-micro')
-const { router, get, post } = require('microrouter')
+const { router, get, post, options } = require('microrouter')
 const { http, schema } = require('./index')
 
 module.exports = router(
   get('/', microGraphiql({ schema, endpointURL: '/' })),
+  options('/', http),
   post('/', http)
 )
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { microGraphql } = require('apollo-server-micro')
 const { mergeSchemas } = require('graphql-tools')
+const { graphql } = require('graphql')
+const { json, send } = require('micro')
 const { graphql: event } = require('./app/event')
 const { schema: location } = require('./app/location')
 const { schema: performer } = require('./app/performer')
@@ -127,9 +129,23 @@ const schema = mergeSchemas({
   })
 })
 
-exports.http = microGraphql((req) => ({
-  schema,
-  context: { viewer: { roles: ['admin'] } }
-}))
+exports.http = async (req, res) => {
+  res.setHeader('Access-Control-Max-Age', 60 * 60 * 24)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', [ 'POST', 'OPTIONS' ])
+  res.setHeader('Access-Control-Allow-Headers', [ 'X-Apollo-Tracing', 'credentials', 'X-Requested-With', 'Access-Control-Allow-Origin', 'X-HTTP-Method-Override', 'Content-Type', 'Authorization', 'Accept' ])
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  if (req.method === 'OPTIONS') {
+    console.log('return early for options')
+    return send(res, 204)
+  }
+  const { query, variables } = await json(req)
+  console.log('return real thing', query, variables)
+  return graphql(schema, query, {}, { viewer: { roles: ['admin'] } }, variables)
+}
+// exports.http = microGraphql((req) => ({
+//   schema,
+//   context: { viewer: { roles: ['admin'] } }
+// }))
 
 exports.schema = schema
