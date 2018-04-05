f061153266ec3e50e0073bedc66930a89cfb7fbf
Seth Tippetts <sethtippetts@gmail.com>
2018-04-05 11:34:39 -0600

    fix: correct formatting for hardcoded location data

--- src/services/location/interface/repository.js
+++ src/services/location/interface/repository.js
@@ -2,30 +2,36 @@ const LOCATIONS = {
   'downtown-slc': {
     id: 'downtown-slc',
     name: 'Wiseguys at the Gateway',
-    city: 'Salt Lake City',
-    state: 'UT',
-    street: '194 South 400 West',
-    postalCode: '84101',
+    address: {
+      address1: '194 South 400 West',
+      locality: 'Salt Lake City',
+      postalCode: '84101',
+      region: 'UT'
+    },
     googleMapsUrl: 'http://www.google.com/maps?f=q&hl=en&ie=UTF8&z=15&om=1&iwloc=addr&q=194 South 400 West Salt Lake City UT 84101 US',
     capacity: 300
   },
   'ogden': {
     id: 'ogden',
     name: 'Wiseguys Comedy Club Ogden',
-    city: 'Ogden',
-    state: 'UT',
-    street: '269 25th St',
-    postalCode: '84401',
+    address: {
+      address1: '269 25th St',
+      locality: 'Ogden',
+      postalCode: '84401',
+      region: 'UT'
+    },
     googleMapsUrl: 'http://www.google.com/maps?f=q&hl=en&ie=UTF8&z=15&om=1&iwloc=addr&q=269 25th St Ogden UT 84401 US',
     capacity: 170
   },
   'jordan-landing': {
     id: 'jordan-landing',
     name: 'Wiseguys Jordan Landing',
-    city: 'West Jordan',
-    state: 'UT',
-    street: '3763 West Center Park Drive',
-    postalCode: '84084',
+    address: {
+      address1: '3763 West Center Park Drive',
+      locality: 'West Jordan',
+      postalCode: '84084',
+      region: 'UT'
+    },
     googleMapsUrl: 'http://www.google.com/maps?f=q&hl=en&ie=UTF8&z=15&om=1&iwloc=addr&q=3763 West Center Park Drive West Jordan UT 84084 US',
     capacity: 300
   }
