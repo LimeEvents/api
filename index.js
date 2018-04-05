4df6b3df974d7148394df6bdf9552dcbc032f679
Seth Tippetts <sethtippetts@gmail.com>
2018-04-05 11:32:26 -0600

    feature: implement Apollo Engine

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const { router, get, post } = require('microrouter')
 const { microGraphql, microGraphiql } = require('apollo-server-micro')
+const { ApolloEngine } = require('apollo-engine')
+const micro = require('micro')
 
 const schema = require('./src/schema')
 
 module.exports = router(
   get('/*', microGraphiql({ schema, endpointURL: '/graphql' })),
-  post('/graphql', microGraphql({ schema, context: { viewer: { roles: ['admin'] } } }))
+  post('/graphql', microGraphql((req) => ({
+    schema,
+    context: { viewer: { roles: ['admin'] } },
+    tracing: true,
+    cacheControl: true
+  })))
 )
+
+const httpServer = micro(module.exports)
+
+const engine = new ApolloEngine({
+  apiKey: process.env.APOLLO_ENGINE_KEY,
+  origins: [{
+    supportsBatch: true
+  }]
+})
+
+engine.listen({
+  port: process.env.PORT,
+  httpServer
+})
