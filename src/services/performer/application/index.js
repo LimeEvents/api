9f58c60eed2819beffcc1a9f2cf6be24a5981c76
Seth Tippetts <sethtippetts@gmail.com>
2018-04-06 08:51:30 -0600

    feature: actually filter with Mongo for performer source

--- src/services/performer/application/index.js
+++ src/services/performer/application/index.js
@@ -8,13 +8,7 @@ module.exports = (repo) => ({
     return domain.get(viewer, { performer })
   },
   async find (viewer, query = {}) {
-    let performers = await repo.find()
-    if (query.filter) {
-      performers = performers.filter((performer) => {
-        if (query.filter.id && !query.filter.id.includes(performer.id)) return false
-        return true
-      })
-    }
+    let performers = await repo.find(query.filter)
     return domain.find(viewer, { performers })
   },
   async register (viewer, performer) {
