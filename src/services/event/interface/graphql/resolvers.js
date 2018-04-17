b3572222e7cf566f23a33e67e83e3a0c696aa96b
Seth Tippetts <sethtippetts@gmail.com>
2018-04-16 18:13:46 -0600

    feature(event): global IDs for Event

--- src/services/event/interface/graphql/resolvers.js
+++ src/services/event/interface/graphql/resolvers.js
@@ -1,16 +1,20 @@
-const { connectionFromPromisedArray } = require('graphql-relay')
+const { fromGlobalId, toGlobalId, connectionFromPromisedArray } = require('graphql-relay')
 const application = require('../application')
 
 module.exports = {
   Query: {
     event: refetchEvent,
-    events (source, args, context, info) {
+    async events (source, args, context, info) {
       if (args.first) args.first = Math.min(args.first, 50)
       if (args.last) args.last = Math.min(args.last, 50)
-      return connectionFromPromisedArray(
+      const { pageInfo, edges } = await connectionFromPromisedArray(
         application.find(context.viewer, args),
         args
       )
+      return {
+        pageInfo,
+        edges: edges.map(({ node }) => ({ node: { ...node, id: toGlobalId('Event', node.id) } }))
+      }
     },
     stream (source, args, { viewer }, info) {
       if (args.first) args.first = Math.min(args.first, 50)
@@ -27,12 +31,12 @@ module.exports = {
       return results
     },
     async cancelEvent (source, { input }, { viewer }, info) {
-      const results = await application.cancel(viewer, input)
+      const results = await application.cancel(viewer, { ...input, id: fromGlobalId(input.id).id })
       results.clientMutationId = input.clientMutationId
       return results
     },
     async rescheduleEvent (source, { input }, { viewer }, info) {
-      const results = await application.reschedule(viewer, input)
+      const results = await application.reschedule(viewer, { ...input, id: fromGlobalId(input.id).id })
       results.clientMutationId = input.clientMutationId
       return results
     }
@@ -48,6 +52,7 @@ module.exports = {
   }
 }
 
-function refetchEvent (source, args, { viewer }, info) {
-  return application.get(viewer, args.id || source.id)
+async function refetchEvent (source, args, { viewer }, info) {
+  const event = await application.get(viewer, args.id || source.id)
+  return { ...event, id: toGlobalId('Event', event.id) }
 }
