4064c7244238a04bbf3a5ac6764ac08d4ecb116b
Seth Tippetts <sethtippetts@gmail.com>
2018-04-20 10:46:16 -0600

    feature(logging): better stack traces from subschemas

--- src/services/location/interface/index.js
+++ src/services/location/interface/index.js
@@ -1,3 +1,5 @@
+const AssertionError = require('assert').AssertionError
+const { onError } = require('apollo-link-error')
 const { SchemaLink } = require('@vivintsolar/graphql-stitch-utils')
 const { schema } = require('./schema')
 const { repository } = require('./repository')
@@ -29,8 +31,18 @@ exports.extensions = {
 }
 
 exports.link = memo(async function () {
+  const errorLink = onError((things) => {
+    const { response, graphQLErrors } = things
+    if (graphQLErrors) {
+      const errors = [].concat(...graphQLErrors.map(formatError))
+      errors.forEach((error) => console.error(error.stack.join('\n    ')))
+      response.errors = graphQLErrors
+        .map(({ originalError }) => originalError)
+        .filter((error) => error instanceof AssertionError)
+    }
+  })
   const _schema = await schema()
-  return new SchemaLink({
+  const schemaLink = new SchemaLink({
     schema: _schema,
     context (operation) {
       const context = operation.getContext().graphqlContext || {}
@@ -42,4 +54,38 @@ exports.link = memo(async function () {
       return { viewer, application: application(repo, services) }
     }
   })
+  return errorLink.concat(schemaLink)
 })
+
+function formatError (error = {}) {
+  if (error.originalError) {
+    return formatError(error.originalError)
+  }
+
+  if (error.errors) {
+    return error.errors.map(formatError)
+  }
+
+  const { locations, message, stack, trace, origin } = error
+  let stackArray
+
+  if (stack) {
+    stackArray = stack.replace(/\n\s+/g, '\n').split('\n')
+  }
+
+  const errorObject = {
+    locations,
+    message,
+    origin
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    errorObject.stack = stackArray
+  }
+
+  if (Array.isArray(trace)) {
+    errorObject.trace = trace.map(formatError)
+  }
+
+  return errorObject
+}
