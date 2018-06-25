f4edb28f052cb753e10c1004e476997166e09b9f
Seth Tippetts <sethtippetts@gmail.com>
2018-06-25 13:51:47 -0600

    fix(interface): update dependency since apollo-server is huge

--- services/product/handler.js
+++ services/product/handler.js
@@ -1,8 +1,16 @@
-const { graphqlLambda } = require('apollo-server-lambda')
-const { sink } = require('./application')
+const { graphql } = require('graphql')
+const { application, getViewer, sink } = require('./application')
 const { schema } = require('./graphql')
 
-exports.graphql = graphqlLambda({ schema })
+exports.graphql = async ({ query, variables, context: { token } }, context, cb) => {
+  try {
+    const viewer = await getViewer(token)
+    const results = await graphql(schema, query, {}, { application: application(viewer) }, variables)
+    return cb(null, results)
+  } catch (ex) {
+    cb(ex)
+  }
+}
 
 exports.sink = async ({ Records }, context, callback) => {
   const results = await Promise.all(Records.map(({ Sns }) => sink(JSON.parse(Sns.Message))))
