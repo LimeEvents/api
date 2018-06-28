272004d160146f1eb19a03efb4dfd1806cc38aaa
Seth Tippetts <sethtippetts@gmail.com>
2018-06-28 15:44:51 -0600

    fix(test): update product tests
    Was previously expecting an array of events, now it expects a product to create

--- services/product/application/commands/addProduct/index.test.js
+++ services/product/application/commands/addProduct/index.test.js
@@ -2,19 +2,22 @@
 
 const { domain } = require('./index')
 
+const FAKE_PRODUCT = {
+  name: 'Google Home'
+}
+
 describe('product', () => {
   describe('add', () => {
     describe('domain', () => {
       it('returns an array of events', () => {
-        const results = domain({}, { product: {} })
+        const results = domain({}, { product: FAKE_PRODUCT })
         expect(results).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({
-              id: expect.any(String),
-              _timestamp: expect.any(Number),
-              _type: 'ProductAdded'
-            })
-          ])
+          expect.objectContaining({
+            id: expect.any(String),
+            created: expect.any(Number),
+            updated: expect.any(Number),
+            ...FAKE_PRODUCT
+          })
         )
       })
     })
