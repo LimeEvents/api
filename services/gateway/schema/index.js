e7c4f938e6c12aa13cd76d3701a9f1cb51f08884
Seth Tippetts <sethtippetts@gmail.com>
2018-06-29 15:38:50 -0600

    fix(gateway): reference correct introspection file

--- services/gateway/schema/index.js
+++ services/gateway/schema/index.js
@@ -1,17 +1,17 @@
 const { mergeSchemas, makeRemoteExecutableSchema } = require('graphql-tools')
 const { buildClientSchema } = require('graphql')
-const product = require('./services/product.json')
+const catalog = require('./services/catalog.json')
 const { links } = require('./links')
 
 const introspections = {
-  product
+  catalog
 }
 exports.links = links
 exports.schema = mergeSchemas({
   schemas: [
     makeRemoteExecutableSchema({
-      link: links.product,
-      schema: buildClientSchema(introspections.product.data)
+      link: links.catalog,
+      schema: buildClientSchema(introspections.catalog.data)
     })
   ]
 })
